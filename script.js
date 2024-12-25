fc328ffbe2032a8eeb4e5ae9e29965bb5f719748
Ronak-Surve <ronaksurve344rs@gmail.com>
2024-12-25 15:54:29 +0530

    new-hello

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function hello() {
-    console.log("Hello from main");
+    console.log("Hello from main");    
+}
+
+function newHello() {
+    console.log("Hello from branch 1");
+    console.log("Hello from branch 2");
 }
